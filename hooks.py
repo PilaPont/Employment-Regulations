055766bbf18ec371356818740506658d9cad225a
Maryam Kia <maryam.kia1984@mail.com>
2021-06-04 12:21:33 +0430

    [IMP] hr_employee_iran_rules, refactor sequences with sequence.mixin

--- hooks.py
+++ hooks.py
@@ -1,17 +1,10 @@
-from odoo import api, SUPERUSER_ID
-
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    last_number = env['hr.employee'].search([], limit=1, order="employee_number desc").employee_number
-    env.ref("hr_employee_iran_rules.employee_number_sequence").number_next = int(last_number) + 1
-
 
 
 def pre_init_hook(cr):
     cr.execute(""" 
-        ALTER TABLE hr_employee ADD COLUMN IF NOT EXISTS employee_number VARCHAR;
+        ALTER TABLE resource_resource ADD COLUMN IF NOT EXISTS employee_number VARCHAR, ADD COLUMN IF NOT EXISTS sequence_number VARCHAR;
     """)
     cr.execute(
         """
-        UPDATE hr_employee SET employee_number="id"  WHERE employee_number IS null OR employee_number='/';
+        UPDATE resource_resource SET employee_number= LPAD(CAST (id AS text), 4, '0') , sequence_number=LPAD(CAST (id AS text), 4, '0') WHERE employee_number IS null OR employee_number='/';
     """)
--- models/resource_resource.py
+++ models/resource_resource.py
@@ -1,12 +1,36 @@
 from odoo import api, fields, models
 
 
-class ResourceCalendar(models.Model):
-    _inherit = 'resource.resource'
+class ResourceResource(models.Model):
+    _name = 'resource.resource'
+    _inherit = ['resource.resource', 'sequence.mixin']
 
-    employee_number = fields.Char(string="Employee Number", default='/', copy=False)
+    _sequence_field = 'employee_number'
+    _sequence_date_field = "create_date"
+
+    employee_number = fields.Char(string="Employee Number", copy=False)
 
     _sql_constraints = [
         ('unique_employee_number', 'UNIQUE(employee_number)',
          'Unfortunately this employee number is already used, please choose a unique one')
     ]
+
+    @api.model
+    def create(self, vals):
+        res = super(ResourceResource, self).create(vals)
+        import logging
+        if 'employee_number' not in vals:
+            logging.critical(res._set_next_sequence())
+
+        return res
+
+    def _get_starting_sequence(self):
+        """Get a default sequence number.
+
+        This function should be overriden by models heriting from this mixin
+        This number will be incremented so you probably want to start the sequence at 0.
+
+        :return: string to use as the default sequence to increment
+        """
+        self.ensure_one()
+        return "0000"
